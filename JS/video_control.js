63864060c51143d00ea33948bc991db75d60c703
HEVV1 <51486822+HEVV1@users.noreply.github.com>
2020-02-01 01:25:18 +0200

    2. Adjustment
    Fix the contacts by ading html code in about.html and work.html code, also add some rensonsivness to contact box.

--- JS/video_control.js
+++ JS/video_control.js
@@ -1,5 +1,5 @@
 // Looping the videos one by one
-var video_list = ["99.mp4", "Alderamin on the sky.mp4", "Apocrypha-Holy To Me.mp4", "Artichoke Hearts.mp4", "Bakemonogatari.mp4", "Fate.mp4", "Fate-Give Me Back My Life.mp4", "Fate-Glory.mp4", "Fate-Phoenix.mp4", "Fate-Rise.mp4", "Fate-Runnin.mp4", "Garou vs Bang.mp4", "Glitter & Gold.mp4", "Light Up My Railgun.mp4", "Play with fire.mp4", "Railgun-Gasoline.mp4", "Railgun-Monster.mp4", "Shichika's Diamond Eyes.mp4", "Steins Gate.mp4", "Stray Dogs.mp4", "Super Hero One Pucnh Man.mp4", "Suzuya and Accelerator.mp4", "Swing Box.mp4", "The Art of War.mp4", "Youjo Senki-Feed The Machine.mp4"];
+var video_list = ["99.mp4", "Alderamin on the sky.mp4", "Apocrypha-Holy To Me.mp4", "Artichoke Hearts.mp4", "Bakemonogatari.mp4", "Fate-Give Me Back My Life.mp4",  "Fate-Phoenix.mp4",  "Fate-Runnin.mp4", "Glitter & Gold.mp4", "Light Up My Railgun.mp4", "Play with fire.mp4", "Railgun-Gasoline.mp4", "Railgun-Monster.mp4", "Shichika's Diamond Eyes.mp4", "Steins Gate.mp4", "Stray Dogs.mp4", "Suzuya and Accelerator.mp4", "Swing Box.mp4", "The Art of War.mp4", "Youjo Senki-Feed The Machine.mp4"];
 var start = 0;
 var storedRanValue = [];
 const video_count = video_list.length;
